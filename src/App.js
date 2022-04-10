e3215ff8a46c247e640c3595fa95099523e8be73
dorian <dorian.schlosser@epitech.eu>
2022-04-10 13:06:29 +0200

    [+] first structure project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <h1>Create your own token using JavaScript</h1>
+        </div>
+    )
+};
+
+export default App;
